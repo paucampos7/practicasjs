0506df1e3d5c303ad653ba6e5397d54a6f0d5687
Paula Campos <paucampos7@gmail.com>
2022-10-30 17:19:04 -0300

    utlimo commit repaso clase tres

---
+++ js/repaso-clase3-c.js
@@ -0,0 +1,5 @@
+// para vincular mi html con js, tengo que crear un id en la etiqueta de mi html, y en js creo la variable de ese id:
+
+let mensajeHtml = document.querySelector ('#mensaje')
+
+console.log (mensajeHtml)
